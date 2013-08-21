94365b494e946e107b14a60399ec0c700bd646a4
Bogdan Cristea <cristeab@gmail.com>
2013-08-21 11:40:52 +0200

    working plugin

--- plugins/process.cpp
+++ plugins/process.cpp
@@ -4,70 +4,8 @@
 #include <QDebug>
 
 #include "process.h"
+#include "process_p.h"
 
-class Process::Private : public QProcess
-{
-    Q_OBJECT
-public:
-    Private(Process *parent);
-    void wait() {
-      if (started) {
-        finishedEvt.exec();
-      }
-    }
-
-    QString program;
-    QStringList arguments;
-    QStringList channels;
-    bool started;
-private slots:
-  void onStarted() {
-    qDebug() << "started";
-    started = true;
-  }
-  void onFinished() {
-    qDebug() << "finished";
-    started = false;
-    if (finishedEvt.isRunning()) {
-      finishedEvt.quit();
-    }
-  }
-private:
-  void loadChannels();
-  Process *q;
-  QEventLoop finishedEvt;
-};
-
-Process::Private::Private(Process *parent)
-    : QProcess(parent)
-    , started(false)
-    , q(parent)
-{
-    connect(this, SIGNAL(started()), q, SIGNAL(started()));
-    connect(this, SIGNAL(started()), this, SLOT(onStarted()));
-    connect(this, SIGNAL(finished(int,QProcess::ExitStatus)), q, SIGNAL(finished(int)));
-    connect(this, SIGNAL(finished(int,QProcess::ExitStatus)), this, SLOT(onFinished()));
-    connect(this, SIGNAL(readyReadStandardOutput()), q, SIGNAL(readyReadStandardOutput()));
-    connect(this, SIGNAL(readyReadStandardError()), q, SIGNAL(readyReadStandardError()));
-    loadChannels();
-}
-
-void Process::Private::loadChannels()
-{
-  QFile file("/etc/mplayer/channels.conf");
-  if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-    qDebug() << "Cannot open file";
-    return;
-  }
-  QSet<QString> channelSet;
-  while (!file.atEnd()) {
-    QString line = file.readLine();
-    QStringList token = line.split(':');
-    channelSet << token[0];
-  }
-  channels = channelSet.toList();
-  channels.sort();
-}
 
 Process::Process(QObject *parent)
     : QObject(parent)
